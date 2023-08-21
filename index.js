96e1f8112b8a70fd38713f6ff9cbdd290df024cd
Rohit Nayal <119488668+rohitnayal12@users.noreply.github.com>
2023-08-21 14:52:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const cors=require("cors")
 const { connection } = require("./db")
 const { userRouter } = require("./routes/userRoute")
 const { postRouter } = require("./routes/postRoute")
@@ -10,6 +11,7 @@ const app=express()
 app.use(express.json())
  app.use("/users",userRouter)
  app.use("/posts",postRouter)
+app.use(cors())
 
 
 
